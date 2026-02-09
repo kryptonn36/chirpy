ca31169fd762972849a3f8b14a1490130af5a473
krypton36 <srai325356@gmail.com>
2026-02-10 01:00:20 +0530

    hanler func

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
-type Server struct{
-	Addr string
-	Handler http.Handler
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
 
-func main(){
-	mux := http.NewServeMux()
-	mux.Handle("/",http.FileServer(http.Dir(".")))
-	server := Server{
-		Addr    : ":8080",
-		Handler : mux ,
-	}
-	http.ListenAndServe(server.Addr,server.Handler)
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
